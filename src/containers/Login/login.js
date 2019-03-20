000cce747f95386ee9c63cbdd301f3455b6bbb75
Mathias Dahl Andersen <mattis417@hotmail.com>
2019-03-20 15:05:37 +0100

    heuhe

---
+++ src/containers/Login/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class login extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <input placeholder="heehehe" />
+        </div>
+    );
+  }
+}
+
+export default login;
