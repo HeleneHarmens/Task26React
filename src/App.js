a5172f620490f9c0908e1950ee22096e2a44337d
Helene Hunding Harmens <helene.hunding.harmens@no.experis.com>
2019-03-22 19:21:19 +0100

    textcolor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 import "./App.css";
 import Myheader from "./components/header/header";
 import Home from "./pages/Index/home";
@@ -10,15 +10,17 @@ import RestaurantPage from "./pages/Restaurant/RestaurantPage";
 
 class App extends Component {
   
+  
   render() {
+    
     return (
       <div className="App">
         <Myheader />
         <Router>
           <ul>
-            <li><Link to="/home">Home</Link></li>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/restaurant">Restaurants</Link></li>
+            <li><NavLink activeStyle={{ color: 'black' }} to="/home">Home</NavLink></li>
+            <li><NavLink activeStyle={{ color: 'black' }} to="/dashboard">Dashboard</NavLink></li>
+            <li><NavLink activeStyle={{ color: 'black' }} to="/restaurant">Restaurants</NavLink></li>
           </ul>
           <Route path="/home" component={Home} />
           <Route path="/restaurant" component={RestaurantPage} />
